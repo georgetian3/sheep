e610078512fa709aa06d55af810ae50e1994f44c
George Tian <github@georgetian.com>
2022-11-07 21:28:06 +0800

    draw button

--- asm/main.asm
+++ asm/main.asm
@@ -107,7 +107,7 @@ WinProc PROC hWnd:DWORD, uMsg:DWORD, wParam:DWORD, lParam:DWORD
         .ELSEIF ebx == WM_CREATE
             Print   OFFSET wm_create
             Print   OFFSET wm_command
-            INVOKE  create_button, hWnd, 1, 100, 100, 1, TILE_WIDTH, TILE_HEIGHT
+            INVOKE  create_button, hWnd, 9, 100, 100, 1, TILE_WIDTH, TILE_HEIGHT
             Print   OFFSET wm_command
             ;INVOKE  play_sound, 0, 0, 0
         .ELSEIF ebx == WM_CLOSE
